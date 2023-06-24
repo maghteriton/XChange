6b8bce5195df1da7530026f80b559a0dfb147695
maghteriton <mert.gorates@hotmail.com>
2023-06-24 13:32:45 +0900

    Kucoin fix exception handling

--- xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/KucoinExceptionClassifier.java
+++ xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/KucoinExceptionClassifier.java
@@ -23,6 +23,10 @@ public final class KucoinExceptionClassifier {
   }
 
   public static RuntimeException classify(KucoinApiException e) {
+    if(e.getMessage() == null) {
+      return new ExchangeException(null, e);
+    }
+
     if (e.getMessage().equalsIgnoreCase("Service unavailable")) {
       return new ExchangeUnavailableException(e.getMessage(), e);
     }
